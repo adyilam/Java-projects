efb033951255c52247f8aff0b083ce77ae7af1b4
Aderajew Amare <aderajewamare@Aderajews-MacBook-Pro.local>
2022-11-01 22:48:01 -0400

    java8 forEach demo

---
+++ LambdaExpression/src/forEach/ForEachExample.java
@@ -0,0 +1,31 @@
+package forEach;
+
+import java.util.*;
+
+public class ForEachExample {
+    public static void main(String[] args) {
+        //add list of products
+        List<Product> productList = Arrays.asList(new Product(1, "TV"), new Product(2, "Book"),
+                new Product(3, "Computer"));
+
+        //iterating over a collection - list
+        System.out.println("== Iterating through LIST == ");
+        productList.forEach(System.out::println);
+
+        //iterating over a collection - set
+        Set<Product> productSet = new HashSet<>(Arrays.asList(new Product(1, "TV"), new Product(2, "Book"),
+                new Product(3, "Computer")));
+        System.out.println("== Iterating through SET ==");
+        productSet.forEach(System.out::println);
+
+        //iterating over a collection - map
+        Map<String, Product> productMap = new HashMap<>();
+        productMap.put("Electronics", new Product(1, "TV"));
+        productMap.put("Hardware", new Product(2, "cycle"));
+        productMap.put("Food", new Product(3, "Yogurt"));
+
+        System.out.println("== Iterating through MAP ==");
+        productMap.forEach((key, value) -> System.out.println(key + " " + value));
+
+    }
+}
---
+++ LambdaExpression/src/forEach/Product.java
@@ -0,0 +1,35 @@
+package forEach;
+
+public class Product {
+    private int productId;
+    private String name;
+
+    public Product(int productId, String name) {
+        this.productId = productId;
+        this.name = name;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
