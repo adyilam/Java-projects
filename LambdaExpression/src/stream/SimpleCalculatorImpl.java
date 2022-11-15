ff063b81f899981d4db436947aba392babcc36ac
Aderajew Amare <aderajewamare@Aderajews-MacBook-Pro.local>
2022-11-15 11:46:10 -0500

    code refactored

--- LambdaExpression/src/stream/SimpleCalculatorImpl.java
+++ LambdaExpression/src/stream/SimpleCalculatorImpl.java
@@ -6,14 +6,20 @@ public class SimpleCalculatorImpl implements SimpleCalculator {
 
     @Override
     public Integer add(Optional<Integer> number1, Optional<Integer> number2) {
-        Integer numberOpt = number1.get();
-        Integer number2Opt = number2.orElse(0);
+        int numberOpt = 0;
+        if (number1.isPresent()) {
+            numberOpt = number1.get();
+        }
+        int number2Opt = number2.orElse(0);
         return numberOpt + number2Opt;
     }
 
     public Integer divide(Optional<Integer> number1, Optional<Integer> number2) {
-        Integer numberOpt = number1.get();
-        Integer number2Opt = number2.orElse(0);
+        int numberOpt = 0;
+        if (number1.isPresent()) {
+            numberOpt = number1.get();
+        }
+        int number2Opt = number2.orElse(0);
         if (number2Opt == 0) {
             number2Opt = 1;
         }
